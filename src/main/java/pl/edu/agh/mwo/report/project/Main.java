868d4549877619924eecc29ef578ae0016a4f03e
mdebski <midebski@student.agh.edu.pl>
2025-06-07 12:15:46 +0200

    initial configuration

---
+++ src/main/java/pl/edu/agh/mwo/report/project/Main.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.mwo.report.project;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bialekruki project!");
+    }
+}
---
+++ src/main/java/pl/edu/agh/mwo/report/project/model/Project.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.mwo.report.project.model;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class Project {
+
+    private List<User> userList;
+}
