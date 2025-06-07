056effeb92a23561265c2b9d6fd5089fb8ba0f7a
zuziaprezi <zuzanna.wawro7@gmail.com>
2025-06-07 15:13:06 +0200

    Report1 implemented

--- src/main/java/pl/edu/agh/mwo/report/project/reports/Raport1.java
+++ src/main/java/pl/edu/agh/mwo/report/project/reports/Raport1.java
@@ -1,20 +1,34 @@
 package pl.edu.agh.mwo.report.project.reports;
 
 import pl.edu.agh.mwo.report.project.model.Project;
+import pl.edu.agh.mwo.report.project.model.Task;
+import pl.edu.agh.mwo.report.project.model.User;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Raport1 {
     public TableReport generate(List<Project> projects) {
-        List<String> headers = Arrays.asList("Name", "Hours", "Project");
+        List<String> headers = Arrays.asList("Name", "Hours");
         List<List<String>> rows = new ArrayList<>();
 
-        for (Project project : projects) {
-            List<String> row = new ArrayList<>();
-            row.add(project.
+        Map<String, Double> userHours = new HashMap<>();
 
+        for (Project project : projects) {
+            for (User user : project.getUserList()) {
+                double totalHours = 0.0;
+                for (Task task : user.getTaskList()) {
+                    totalHours += task.getTimeSpentOnTheTask();
+                }
+
+                String userName = user.getName();
+
+                userHours.put(userName, userHours.getOrDefault(userName, 0.0) + totalHours);
+
+            }
+        }
+
+        for (Map.Entry<String, Double> entry : userHours.entrySet()) {
+            rows.add(Arrays.asList(entry.getKey(), String.format("%.2f", entry.getValue())));
         }
 
         return new TableReport("Raport 1", headers, rows);
