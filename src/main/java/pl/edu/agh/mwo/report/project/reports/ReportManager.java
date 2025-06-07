31c6f04594d0527a0f9ceeebc63a4b1755fd6b70
Krystian <milanello07@gmail.com>
2025-06-07 13:19:58 +0200

    first commit

--- src/main/java/pl/edu/agh/mwo/report/project/reports/ReportManager.java
+++ src/main/java/pl/edu/agh/mwo/report/project/reports/ReportManager.java
@@ -1,4 +1,4 @@
 package pl.edu.agh.mwo.report.project.reports;
 
-public class ReportMenager {
+public class ReportManager {
 }
--- src/main/java/pl/edu/agh/mwo/report/project/reports/ReportRow.java
+++ src/main/java/pl/edu/agh/mwo/report/project/reports/ReportRow.java
@@ -1,4 +1,11 @@
 package pl.edu.agh.mwo.report.project.reports;
 
+import java.util.HashMap;
+
 public class ReportRow {
+    private HashMap<String, Object> dataMap = new HashMap<>();
+
+    public HashMap<String, Object> getDataMap() {
+        return dataMap;
+    }
 }
