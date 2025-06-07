8ae718a6dd609c20f38dc7c5db04830fd3882284
PiotrPoloczek <piotr@PiotrPoloczeks-MacBook-Pro.local>
2025-06-07 14:35:40 +0200

    remove unecessary import in Main

--- src/main/java/pl/edu/agh/mwo/report/project/Main.java
+++ src/main/java/pl/edu/agh/mwo/report/project/Main.java
@@ -1,16 +1,9 @@
 package pl.edu.agh.mwo.report.project;
 
-import pl.edu.agh.mwo.report.project.report.ReportType;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Bialekruki project!");
-
 
         InputManager inputManager = new InputManager(args);
 
