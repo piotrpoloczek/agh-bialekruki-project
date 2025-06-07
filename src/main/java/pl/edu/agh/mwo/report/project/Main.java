e0c97696eb8497c13995d136368110a63d80ada2
PiotrPoloczek <piotr@PiotrPoloczeks-MacBook-Pro.local>
2025-06-07 15:15:58 +0200

    fix conflicts

--- src/main/java/pl/edu/agh/mwo/report/project/Main.java
+++ src/main/java/pl/edu/agh/mwo/report/project/Main.java
@@ -7,13 +7,7 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-      
-      
-//         System.out.println(
-//                 ExcelParser.parseExcelToProjectList(
-//                         Path.of("/home/students/m/d/mdebski/IdeaProjects/agh-bialekruki-project/"
-//                                 + "src/main/resources/input/reporter-dane/2012/01")
-//                 ));
+
         System.out.println("Welcome to Bialekruki project!");
 
         InputManager inputManager = new InputManager(args);
--- src/main/java/pl/edu/agh/mwo/report/project/reports/TableReport.java
+++ src/main/java/pl/edu/agh/mwo/report/project/reports/TableReport.java
@@ -2,28 +2,28 @@ package pl.edu.agh.mwo.report.project.reports;
 
 import java.util.List;
 
-public class TableReport{
+public class TableReport {
 
-private String name;
-private List<String> headers;
-private List<List<String>> values;
+    private String name;
+    private List<String> headers;
+    private List<List<String>> values;
 
-public TableReport(String name, List<String> headers, List<List<String>> values) {
-    this.name = name;
-    this.headers = headers;
-    this.values = values;
-}
+    public TableReport(String name, List<String> headers, List<List<String>> values) {
+        this.name = name;
+        this.headers = headers;
+        this.values = values;
+    }
 
-public String getName() {
-    return name;
-}
+    public String getName() {
+        return name;
+    }
 
-public List<String> getHeaders() {
-    return headers;
-}
+    public List<String> getHeaders() {
+        return headers;
+    }
 
-public List<List<String>> getValues() {
-    return values;
+    public List<List<String>> getValues() {
+        return values;
     }
 }
 
