c0daffe1784a762ddbb8be5900c318ad17624f09
Krystian <milanello07@gmail.com>
2025-06-07 13:49:45 +0200

    another commit

--- src/main/java/pl/edu/agh/mwo/report/project/reports/Raport1.java
+++ src/main/java/pl/edu/agh/mwo/report/project/reports/Raport1.java
@@ -1,6 +1,25 @@
 package pl.edu.agh.mwo.report.project.reports;
 
-public class Raport1 {
+import pl.edu.agh.mwo.report.project.model.Project;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Raport1 {
+    public TableReport generate(List<Project> projects) {
+        List<String> headers = Arrays.asList("Name", "Hours", "Project");
+        List<List<String>> rows = new ArrayList<>();
+
+        for (Project project : projects) {
+            List<String> row = new ArrayList<>();
+            row.add(project.getUserFullName());
+            row.add(String.valueOf(project.getHours()));
+            row.add(project.getName());
+            rows.add(row);
+        }
+
+        return new TableReport("Raport 1", headers, rows);
+    }
 
 }
