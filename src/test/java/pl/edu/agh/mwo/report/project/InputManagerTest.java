9403395c6aff5f6c8d246fb965a529d27f8fcb29
PiotrPoloczek <piotr@PiotrPoloczeks-MacBook-Pro.local>
2025-06-08 10:17:41 +0200

    add the test for input manager

---
+++ src/test/java/pl/edu/agh/mwo/report/project/InputManagerTest.java
@@ -0,0 +1,17 @@
+package pl.edu.agh.mwo.report.project;
+
+import org.junit.Test;
+
+public class InputManagerTest {
+
+
+    @Test
+    public void testConstructor(){
+
+        myClass class = new myClass(new String[]{"file1", "file2"});
+        /* Your assertions here */
+
+    }
+
+
+}
