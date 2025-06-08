45d2c44bb69a9b3713bbc67db087f186963064e6
Weronika <wer.szulinska@gmail.com>
2025-06-08 14:26:08 +0200

    Raport 4

--- src/main/java/pl/edu/agh/mwo/report/project/reports/Top10Report.java
+++ src/main/java/pl/edu/agh/mwo/report/project/reports/Top10Report.java
@@ -1,10 +1,44 @@
 package pl.edu.agh.mwo.report.project.reports;
 
+import pl.edu.agh.mwo.report.project.model.ErrorFromExcelParser;
 import pl.edu.agh.mwo.report.project.model.Project;
+import pl.edu.agh.mwo.report.project.model.Task;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Top10Report implements GenerateRaport {
+    public TableReport generate(List<Project> projects) {
+        List<String> headers = Arrays.asList("Activity", "Hours");
+        List<List<String>> rows = new ArrayList<>();
+        List<ErrorFromExcelParser> errors = new ArrayList<>();
+
+        Map<String, Double> activityHours = new HashMap<>();
+
+        for (Project project : projects) {
+            errors.addAll(project.getErrorFromExcelParserList());
+
+            for (Task task : project.getTaskList()) {
+                String taskName = task.getName();
+                double hours = task.getTimeSpentOnTheTask();
+                activityHours.put(taskName, activityHours.getOrDefault(taskName, 0.0) + hours);
+            }
+        }
+
+        Map<String,Double> topTen =
+                activityHours.entrySet().stream()
+                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                        .limit(10)
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+        for (Map.Entry<String, Double> entry : topTen.entrySet()) {
+            rows.add(Arrays.asList(entry.getKey(), String.format("%.2f", entry.getValue())));
+        }
+
+        return new TableReport("Raport 4", headers, rows, errors);
+    }
+
     @Override
     public TableReport generateReport() {
         return null;
--- src/main/java/pl/edu/agh/mwo/report/project/tests/ExcelPrinterTest.java
+++ src/main/java/pl/edu/agh/mwo/report/project/tests/ExcelPrinterTest.java
@@ -1,45 +1,45 @@
-package pl.edu.agh.mwo.report.project.tests;
-
-
-import org.junit.Test;
-import pl.edu.agh.mwo.report.project.reports.ExcelPrinter;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-public class ExcelPrinterTest {
-
-    @Test
-    public void testPrintExcel() throws IOException {
-
-        List<String> headers = new ArrayList<>();
-        headers.add("Pracownik");
-        headers.add("Liczba godzin");
-
-        List<List<String>> rows = new ArrayList<>();
-        rows.add(List.of("Jan", "12"));
-        rows.add(List.of("Krzysztof", "15"));
-        rows.add(List.of("Marek", "10"));
-
-        String title = "Raport_1";
-
-        ExcelPrinter printer = new ExcelPrinter(title, headers, rows);
-        printer.printReportOne();
-
-        List<String> headers2 = new ArrayList<>();
-        headers2.add("Projekt");
-        headers2.add("Liczba godzin");
-
-        List<List<String>> rows2 = new ArrayList<>();
-        rows2.add(List.of("Projekt 1", "200"));
-        rows2.add(List.of("Projekt 2", "300"));
-        rows2.add(List.of("Projekt 3", "20"));
-
-        String title2 = "Raport_2";
-
-        ExcelPrinter printer2 = new ExcelPrinter(title2, headers2, rows2);
-        printer2.printReportTwo();
-    }
-
-}
+//package pl.edu.agh.mwo.report.project.tests;
+//
+//
+//import org.junit.Test;
+//import pl.edu.agh.mwo.report.project.reports.ExcelPrinter;
+//
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class ExcelPrinterTest {
+//
+//    @Test
+//    public void testPrintExcel() throws IOException {
+//
+//        List<String> headers = new ArrayList<>();
+//        headers.add("Pracownik");
+//        headers.add("Liczba godzin");
+//
+//        List<List<String>> rows = new ArrayList<>();
+//        rows.add(List.of("Jan", "12"));
+//        rows.add(List.of("Krzysztof", "15"));
+//        rows.add(List.of("Marek", "10"));
+//
+//        String title = "Raport_1";
+//
+//        ExcelPrinter printer = new ExcelPrinter(title, headers, rows);
+//        printer.printReportOne();
+//
+//        List<String> headers2 = new ArrayList<>();
+//        headers2.add("Projekt");
+//        headers2.add("Liczba godzin");
+//
+//        List<List<String>> rows2 = new ArrayList<>();
+//        rows2.add(List.of("Projekt 1", "200"));
+//        rows2.add(List.of("Projekt 2", "300"));
+//        rows2.add(List.of("Projekt 3", "20"));
+//
+//        String title2 = "Raport_2";
+//
+//        ExcelPrinter printer2 = new ExcelPrinter(title2, headers2, rows2);
+//        printer2.printReportTwo();
+//    }
+//
+//}
