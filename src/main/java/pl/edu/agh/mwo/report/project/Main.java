10b49798f7e0d5d439963998aa2c461c61a29f94
Krystian <milanello07@gmail.com>
2025-06-08 13:38:07 +0200

    third report updates

--- src/main/java/pl/edu/agh/mwo/report/project/Main.java
+++ src/main/java/pl/edu/agh/mwo/report/project/Main.java
@@ -2,6 +2,10 @@ package pl.edu.agh.mwo.report.project;
 
 
 import pl.edu.agh.mwo.report.project.model.Project;
+import pl.edu.agh.mwo.report.project.reports.RaportFactory;
+import pl.edu.agh.mwo.report.project.reports.ReportType;
+import pl.edu.agh.mwo.report.project.reports.TableReport;
+
 import java.io.IOException;
 import java.util.List;
 
