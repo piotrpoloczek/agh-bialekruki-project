e1e7b2d035a043b8d6f7003f85aaf64051563702
PiotrPoloczek <piotr@PiotrPoloczeks-MacBook-Pro.local>
2025-06-08 12:16:25 +0200

    add the report manager

---
+++ src/main/java/pl/edu/agh/mwo/report/project/reports/FilterByTagReport.java
@@ -0,0 +1,17 @@
+package pl.edu.agh.mwo.report.project.reports;
+
+import pl.edu.agh.mwo.report.project.model.Project;
+
+import java.util.List;
+
+public class FilterByTagReport implements GenerateRaport {
+    @Override
+    public TableReport generateReport() {
+        return null;
+    }
+
+    @Override
+    public void setInputData(List<Project> projects) {
+
+    }
+}
