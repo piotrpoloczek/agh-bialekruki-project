7240c36f171824339bf14f5b394764a3d303de06
zuziaprezi <zuzanna.wawro7@gmail.com>
2025-06-08 09:26:31 +0200

    Class name modified to UserHoursAllProjects

--- src/main/java/pl/edu/agh/mwo/report/project/reports/UserHoursAllProjects.java
+++ src/main/java/pl/edu/agh/mwo/report/project/reports/UserHoursAllProjects.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
-public class Raport1 {
+public class UserHoursAllProjects {
     public TableReport generate(List<Project> projects) {
         List<String> headers = Arrays.asList("Name", "Hours");
         List<List<String>> rows = new ArrayList<>();
