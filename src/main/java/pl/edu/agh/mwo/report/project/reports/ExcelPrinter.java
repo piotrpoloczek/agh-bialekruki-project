7b10fb57248d5aca9690cff36b63d98a2cc51337
Michal Debski <michal.d9777@gmail.com>
2025-06-08 11:51:10 +0200

    add previously removed class

--- src/main/java/pl/edu/agh/mwo/report/project/reports/ExcelPrinter.java
+++ src/main/java/pl/edu/agh/mwo/report/project/reports/ExcelPrinter.java
@@ -1,11 +1,23 @@
 package pl.edu.agh.mwo.report.project.reports;
 
+import lombok.Getter;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
+@Getter
 public class ExcelPrinter {
 
     private final List<String> headers;
@@ -18,19 +30,7 @@ public class ExcelPrinter {
         this.rows = rows;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public List<String> getHeaders() {
-        return headers;
-    }
-
-    public List<List<String>> getRows() {
-        return rows;
-    }
-
-    public String madePackage() throws FileNotFoundException {
+    public String madePackage() {
         String folderName = "reports";
         File dir = new File(folderName);
 
@@ -55,4 +55,47 @@ public class ExcelPrinter {
         return now.format(format);
     }
 
+    private void createPackage() throws IOException {
+
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet(getTitle());
+        Map<String, Object[]> data = new TreeMap<>();
+
+        List<String> headers = getHeaders();
+        data.put("1", headers.toArray(new Object[0]));
+
+        for (int i = 0; i < getRows().size(); i++) {
+            data.put(String.valueOf(i + 2), getRows().get(i).toArray());
+        }
+
+        Set<String> keyset = data.keySet();
+        int rownum = 0;
+        for (String key : keyset) {
+            Row row = sheet.createRow(rownum++);
+            Object[] objArr = data.get(key);
+            int cellnum = 0;
+            for (Object obj : objArr) {
+                Cell cell = row.createCell(cellnum++);
+                if (obj instanceof String) {
+                    cell.setCellValue((String) obj);
+                } else if (obj instanceof Integer) {
+                    cell.setCellValue((Integer) obj);
+                }
+            }
+        }
+        String folderName = madePackage();
+        String currentDate = currentDate();
+
+        FileOutputStream out = new FileOutputStream(folderName + "/" + getTitle() + "_" + currentDate + ".xlsx");
+        workbook.write(out);
+        out.close();
+    }
+
+    public void printReportOne() throws IOException {
+        createPackage();
+    }
+
+    public void printReportTwo() throws IOException {
+        createPackage();
+    }
 }
